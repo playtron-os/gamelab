ca32e41a37d947f5b31d51132eb7d326aac4d5bb
Mathieu Comandon <strycore@gmail.com>
2024-11-20 12:47:30 -0800

    Move library loading to library screen instead of the router

--- src/hooks/app-library/index.ts
+++ src/hooks/app-library/index.ts
@@ -1,6 +1,5 @@
 export * from "./use-app-download-actions";
 export * from "./use-app-library-actions";
-export * from "./use-app-library-manager";
 export * from "./use-app-terminate-actions";
 export * from "./use-app-uninstall-actions";
 export * from "./use-app-launch";
--- src/screens/library/index.tsx
+++ src/screens/library/index.tsx
@@ -1,13 +1,95 @@
 import React from "react";
+import { usePlayserve } from "@/hooks";
+import { MessageType } from "@/types/playserve/message";
+
+import { useAppActions, useAppSelector } from "@/redux/store";
+import {
+  setApps,
+  updateInstalledApps,
+  updateAppStatus,
+  setQueue,
+  setError,
+  setLoadingProgress,
+  setAppDownloadProgress,
+  selectAppLibraryAppsState
+} from "@/redux/modules";
 import { AppLibrary } from "../../components";
 import { SidePanel } from "@/components/side-panel/side-panel";
-import { selectAppLibraryAppsState } from "@/redux/modules";
-import { useAppSelector } from "@/redux/store";
+
 import { SubmissionsContextProvider } from "@/context/submissions-context";
 import { Sidebar } from "@/components/sidebar/sidebar";
+import { useMount } from "ahooks";
+import { useAppLibraryActions } from "@/hooks/app-library/use-app-library-actions";
 
 export const LibraryScreen = () => {
+  const {
+    setApps: setAppsDispatch,
+    updateInstalledApps: updateInstalledAppsDispatch,
+    updateAppStatus: updateAppStatusDispatch,
+    setQueue: setQueueDispatch,
+    setError: setErrorDispatch,
+    setLoadingProgress: setLoadingProgressDispatch,
+    setAppDownloadProgress: setAppDownloadProgressDispatch
+  } = useAppActions({
+    setApps,
+    updateInstalledApps,
+    updateAppStatus,
+    setQueue,
+    setError,
+    setLoadingProgress,
+    setAppDownloadProgress
+  });
+  const { fetchLibraryApps, fetchQueue } = useAppLibraryActions();
+  useMount(() => {
+    fetchLibraryApps(false);
+    fetchQueue();
+  });
+
   const apps = useAppSelector(selectAppLibraryAppsState);
+
+  usePlayserve({
+    onMessage: (message) => {
+      if (
+        [MessageType.AppLibraryGet, MessageType.AppLibraryUpdate].includes(
+          message.message_type
+        )
+      ) {
+        if (message.status === 200) {
+          setAppsDispatch(message.body);
+        } else {
+          setErrorDispatch(message.body.message);
+        }
+      } else if (
+        message.message_type === MessageType.AppGetInstalled ||
+        message.message_type === MessageType.AppGetInstalledUpdate
+      ) {
+        if (message.status === 200) {
+          updateInstalledAppsDispatch(message.body.installed_apps);
+        }
+      } else if (
+        message.message_type === MessageType.QueueDownloadGet ||
+        message.message_type === MessageType.QueueDownloadGetUpdate
+      ) {
+        if (message.status === 200) {
+          setQueueDispatch(message.body.downloads);
+        }
+      } else if (message.message_type === MessageType.AppDownloadProgress) {
+        if (message.status === 200) {
+          setAppDownloadProgressDispatch(message.body);
+        }
+      } else if (message.message_type === MessageType.AppGetOwnedProgress) {
+        if (message.status === 200) {
+          setLoadingProgressDispatch({
+            [message.body.provider]: message.body.progress
+          });
+        }
+      } else if (message.message_type === MessageType.AppStatusUpdate) {
+        if (message.status === 200) {
+          updateAppStatusDispatch(message.body);
+        }
+      }
+    }
+  });
 
   return (
     <>
