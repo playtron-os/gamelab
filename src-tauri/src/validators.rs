b756142c1cad34f4857d826732d9e89f57040a57
Mathieu Comandon <strycore@gmail.com>
2024-12-10 19:52:43 -0800

    Rust code linting

--- src-tauri/src/validators.rs
+++ src-tauri/src/validators.rs
@@ -1,10 +1,8 @@
-use serde_json;
-
 #[tauri::command]
 pub async fn validate_json(data: &str) -> Result<String, String> {
     let result = serde_json::from_str::<serde_json::Value>(data);
     match result {
-        Ok(_) => return Ok("".to_string()),
-        Err(e) => return Ok(e.to_string()),
+        Ok(_) => Ok("".to_string()),
+        Err(e) => Ok(e.to_string()),
     }
 }
