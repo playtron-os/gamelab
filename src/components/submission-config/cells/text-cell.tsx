829777c70b9bb311798e75feccbb4141fa8bf4ff
Mathieu Comandon <strycore@gmail.com>
2024-12-23 16:29:51 -0800

    Convert submission table to cards

--- src/components/submission-config/cells/text-cell.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { CellContext } from "@tanstack/react-table";
-import { Submission } from "@/types/submission";
-
-export const TextCell = (info: CellContext<Submission, string>) => (
-  <div className="my-3 px-2 overflow-ellipsis text-nowrap font-bold">
-    {info.getValue()}
-  </div>
-);
--- src/components/submission-config/submission-action-menu.tsx
+++ src/components/submission-config/submission-action-menu.tsx
@@ -3,14 +3,17 @@ import { t } from "@lingui/macro";
 import { useAppSelector } from "@/redux/store";
 import { selectAuthState, AuthState } from "@/redux/modules/auth";
 import { Dropdown, DotsVertical, styles } from "@playtron/styleguide";
-import { CellContext } from "@tanstack/react-table";
+
 import { useSubmissionsContext } from "@/context/submissions-context";
 import { Submission } from "@/types";
 
-export type SubmissionCellContext = CellContext<Submission, unknown>;
+interface SubmissionMenuProps {
+  submission: Submission;
+}
 
-export const SubmissionActionCell = (info: SubmissionCellContext) => {
-  const submission = info.row.original;
+export const SubmissionActionMenu: React.FC<SubmissionMenuProps> = ({
+  submission
+}) => {
   const { email } = useAppSelector(selectAuthState) as AuthState;
 
   const {
@@ -93,7 +96,7 @@ export const SubmissionActionCell = (info: SubmissionCellContext) => {
       data={appActions}
       triggerElem={
         <DotsVertical
-          data-testid={`app-action-cell-${info.row.index}`}
+          data-testid={`app-action-cell-${submission.item_id}`}
           fill={styles.variablesDark.fill.white}
           className="cursor-pointer"
         />
