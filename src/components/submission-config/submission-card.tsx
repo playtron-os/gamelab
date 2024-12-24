94804bba70055977f8d34ef138311d76d74a64b2
Mathieu Comandon <strycore@gmail.com>
2024-12-23 16:29:51 -0800

    Convert submission table to cards

---
+++ src/components/submission-config/submission-card.tsx
@@ -0,0 +1,52 @@
+import React from "react";
+import classNames from "classnames";
+import { SubmissionCategory } from "@/constants";
+import { Submission } from "@/types";
+import { SubmissionActionMenu } from "./submission-action-menu";
+
+export const SubmissionCard: React.FC<{
+  submission: Submission;
+  selectedItemId: string | null;
+  setSelectedItemId: (itemId: string) => void;
+}> = ({ submission, selectedItemId, setSelectedItemId }) => {
+  return (
+    <div
+      key={submission.item_id}
+      className={classNames(
+        "flex flex-col w-full h-[60px] rounded-lg cursor-pointer my-2",
+        selectedItemId === submission.item_id
+          ? " outline-2 outline-double bg-[--fill-default]"
+          : "bg-[--fill-subtle]"
+      )}
+      onClick={() => setSelectedItemId(submission.item_id)}
+    >
+      <div className="flex items-center">
+        <div className="flex-grow my-3 px-2 overflow-ellipsis text-nowrap font-bold">
+          <span>{submission.name}</span>
+          <span
+            className={classNames(
+              "rounded-sm px-2 py-1 ms-2 text-xs",
+              submission.submission_category === SubmissionCategory.Official
+                ? "bg-[--feedback-success-primary]"
+                : "bg-[--feedback-success-tertiary]"
+            )}
+          >
+            {submission.submission_category}
+          </span>
+          <br />
+          <span className="text-xs">{submission.description}</span>
+        </div>
+
+        <div className="flex-shrink">
+          <span className="text-sm">{submission.author_name}</span> <br />
+          <span className="text-xs">{submission.updated_date}</span>
+        </div>
+        <div className="flex-shrink-0">
+          <span className="px-5 py-0">
+            <SubmissionActionMenu submission={submission} />
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+};
--- src/components/submission-config/submissions-list.tsx
+++ src/components/submission-config/submissions-list.tsx
@@ -1,8 +1,6 @@
-import React, { useCallback, useEffect, useState } from "react";
+import React, { useCallback } from "react";
 import { usePlayserve } from "@/hooks";
-
-import { Table } from "@playtron/styleguide";
-import { columns } from "./submissions-table-config";
+import { SubmissionCard } from "./submission-card";
 import {
   Submission,
   SubmissionItemType,
@@ -22,26 +20,23 @@ interface SubmissionsListProps {
 export const SubmissionsList: React.FC<SubmissionsListProps> = ({
   submissions,
   submissionType,
-  configFilter,
+  // configFilter,
   selectedItemId,
   setSelectedItemId,
   onClose
 }) => {
   const { sendMessage } = usePlayserve();
-  const [selectedIndexes, setSelectedIndexes] = useState<string[]>([]);
-  const onSelectedIdsChange = useCallback(
-    (index: string) => {
-      try {
-        const subIndex = parseInt(index);
-        const selectedSubmission = submissions[subIndex] as Submission;
-        setSelectedItemId(selectedSubmission.item_id);
 
+  const setDefaultSubmission = useCallback(
+    (submission: Submission) => {
+      try {
+        setSelectedItemId(submission.item_id);
         const setSelectedSubmissionMessage = getMessage(
           MessageType.SubmissionSetDefault,
           {
-            app_id: selectedSubmission.app_id,
+            app_id: submission.app_id,
             item_type: submissionType,
-            item_id: selectedSubmission.item_id
+            item_id: submission.item_id
           }
         );
         sendMessage(setSelectedSubmissionMessage)().then((res) => {
@@ -57,25 +52,22 @@ export const SubmissionsList: React.FC<SubmissionsListProps> = ({
     [submissions, setSelectedItemId, onClose]
   );
 
-  useEffect(() => {
-    for (let index = 0; index < submissions.length; index++) {
-      if (submissions[index].item_id === selectedItemId) {
-        setSelectedIndexes([index.toString()]);
-        return;
-      }
-    }
-    return setSelectedIndexes([]);
-  }, [submissions, selectedItemId, setSelectedIndexes]);
-
   return (
     <div className="flex-grow px-8 overflow-scroll">
-      <Table
-        data={submissions}
-        globalFilter={configFilter}
-        columns={columns}
-        selectedIds={selectedIndexes}
-        onSelectedIdsChange={onSelectedIdsChange}
-      />
+      {submissions.map((submission) => {
+        return (
+          <SubmissionCard
+            key={submission.item_id}
+            submission={submission}
+            selectedItemId={selectedItemId}
+            setSelectedItemId={() => setDefaultSubmission(submission)}
+          />
+        );
+      })}
     </div>
   );
+
+  // globalFilter={configFilter}
+  // selectedIds={selectedIndexes}
+  // onSelectedIdsChange={onSelectedIdsChange}
 };
